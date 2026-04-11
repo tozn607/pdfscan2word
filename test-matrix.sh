7bb54803ac5ecf5b7aab071dd1fa9b72457d8e3c
Anh <tozn@local>
2026-04-11 22:27:45 +0700

    workflow: fix again

--- test-matrix.sh
+++ test-matrix.sh
@@ -1,20 +1,3 @@
 MATRIX="[]"
-RUN_ANY="false"
-IN_WIN="true"
-IN_MAC="false"
-IN_ARM="true"
-
-if [ "$IN_WIN" == "true" ]; then 
-  MATRIX=$(echo $MATRIX | jq '. += [{"os": "windows-latest", "suffix": "Windows"}]')
-  RUN_ANY="true"
-fi
-if [ "$IN_MAC" == "true" ]; then 
-  MATRIX=$(echo $MATRIX | jq '. += [{"os": "macos-15-intel", "suffix": "macOS-x86_64"}]')
-  RUN_ANY="true"
-fi
-if [ "$IN_ARM" == "true" ]; then 
-  MATRIX=$(echo $MATRIX | jq '. += [{"os": "macos-15", "suffix": "macOS-arm64"}]')
-  RUN_ANY="true"
-fi
-echo "$MATRIX"
-echo "run_any=$RUN_ANY"
+MATRIX=$(echo $MATRIX | jq '. += [{"os": "windows-latest", "suffix": "Windows"}]')
+echo "matrix={\"include\":$MATRIX}"
