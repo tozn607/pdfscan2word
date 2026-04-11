95c7bdb1e1b90961dfdd41bc55f0277f5a2c2ba8
Anh <tozn@local>
2026-04-11 22:04:00 +0700

    main: downgrade version to 2.0.0

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ try:
 except ImportError:
     print("[!] CẢNH BÁO: Chưa cài đặt pillow-heif. Không thể đọc file HEIC.")
 
-CURRENT_VERSION = "2.0.1"
+CURRENT_VERSION = "2.0.0"
 GITHUB_API_URL = "https://api.github.com/repos/tozn607/pdfscan2word/releases/latest"
 RELEASES_URL = "https://github.com/tozn607/pdfscan2word/releases"
 
